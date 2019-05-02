8e5e99e405dac5b2873135b49ff9b80f7fd45381
MarcusB2105 <marcusb2105@gmail.com>
2019-05-02 21:24:46 +0200

    reiskörner

--- Schachbrett.py
+++ Schachbrett.py
@@ -2,5 +2,5 @@ summe = 0
 for feld in range(64):
     reiskorn = 2**feld
     summe=summe+reiskorn
-    print("Feld {}. = {} Reiskörner und damit insgesamt {} Reiskörner" \
+    print("Feld {}. = {:>30,} Reiskörner und damit insgesamt {:>30,} Reiskörner" \
             .format(feld+1,reiskorn,summe))
